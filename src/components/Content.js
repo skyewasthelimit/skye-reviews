649d4b4da28734b1b6e003bf55d33bf66a0fe6cf
Skye McLean <skyemclean87@gmail.com>
2024-01-29 15:49:19 -0800

    Initial Commit

---
+++ src/components/Content.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='reviews'>
+
+      <div className='entry'>
+        &nbsp;&nbsp;&nbsp;Developed by Sabotage Studio, Sea of Stars was another crowd funded Kickstarter success and a solid
+        standout entry in the heavy hitting line up of 2024 game releases.
+      <br></br>
+      <h3>Gameplay</h3>
+      &nbsp;&nbsp;&nbsp;A heavily inspired title, Sea of Stars draws influence from
+      excellent retro titles such as Chrono Trigger and Super Mario RPG. You can expect
+      to see enemies on the area maps and engage in turn-based combat directly
+      on the scene. The usage of timed hits and defends makes for engaging and often tense
+      battles. Though rarely did I feel the need to grind, you are able to respawn enemies by leaving
+      and re-entering an area. Players can expect to level up and make choices in 
+      building their characters abilities to their liking. The game does offer
+      open-world exploration but it is designed as a linear experience. You will utilize
+      items, gear and cooking recipes to make the best of your adventures. Sea of Stars
+      has a large amount of boss fights that are fair and balanced and will put your abilities to the
+      test. Similar to Chrono Trigger you can use combo attacks with your party members however, unlike
+      Chrono Trigger you can swap party members in and out during combat much like you would in Pokemon.
+      My personal feelings are I wish there were a few more abilities to use because I often ended up only
+      using a few main abilities myself for each character. You can take advantage of a bonfire mechanic to
+      replenish your party's health and mana.
+      <br></br>
+      <h3>Music & Sound</h3>
+      &nbsp;&nbsp;&nbsp;Sabotage studios was able to get legendary composer Yasunori Mitsuda to contribute a number of
+      tracks to Sea of Stars which really helped encapsulate the nostalgic feel and vibe of the game.
+      In combination with in-house composer Eric W. Brown the music is on point and gives a great soundscape
+      that is a pleasure to listen to through each area and boss fight. You never feel the music unsuiting and you're always
+      excited to hear the next track when it comes in. The sounds are satisfying and every timed hit is punctuated by
+      a timing indicator noise that sits just right in the mix. I loved the sounds of Sea of Stars and the music especially
+      compliments this beautifully rendered game.
+      <br></br>
+      <h3>Story</h3>
+      &nbsp;&nbsp;&nbsp;Taking place within The Messenger universe, Sea of Stars story visits a few different themes including: grief, growth, friendship, pirates and of course the 
+      always cliche - saving the world. Your adventure will take you to a variety of locations and biomes including snow, dessert, tropical islands and
+      deep deep under the ocean and many more! You follow the story of two heroes named Valere and Zale
+      who possess the powers of the moon and the sun which they harness to fight evil forces. Your journey is adjoined byseveral companions you meet along the way.
+      I think my personal favourite was Garl who is the childhood friend of the main characters and is the main chef of the party and an enthusiastic and positive influence
+      on the group throughout. Sea of Stars does a great job of incorporating a few mind bending elements such as time travel along the way to create a rich
+      and entertaining experience for the player. The storytelling is strong and delivers the point but can often be a bit overly cheery and some of the character development
+      was not mature enough for my taste but there are many parts that left a great impact as a whole.
+      <h2>Final Rating: 7.5 / 10</h2>
+      &nbsp;&nbsp;&nbsp;Overall I had an awesome time with Sea of Stars and it has plenty of content to offer including an end game arc for players wanting a bit more. A genuinely 
+      fun game with great retro appeal for the JRPG lover.
+
+      
+      </div>
+      <div className='entry-image'>
+      </div>
+      <img src="https://image.api.playstation.com/vulcan/ap/rnd/202303/0921/500cf443c7a7e8a36a882c393ecb1d4a20b9c04bfef9ad57.png" alt="Game"></img>
+      <div className='entry-title'>
+        Sea of Stars
+      </div>
+
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Navbar = () => {
+
+
+
+  return (
+    <div className='navbar'>
+      <div className='navbar-wrapper'>
+        <form id="searchForm">
+        <input id="searchInput" className="search-input" type="text" placeholder='Find a game...'></input>
+        </form>
+      <i class="fa-solid fa-magnifying-glass"></i>
+        <div className='nav-link'>Home</div>
+        <a href="https://skyewasthelimit.netlify.app/" rel="noreferrer" target='_blank'><div className='nav-link'>About</div></a>
+        <a href="mailto:skyemclean87@gmail.com"><div className='nav-link'>Contact</div></a>
+        <div className='review-btn'>Review My Game</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
