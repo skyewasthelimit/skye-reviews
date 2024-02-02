052732440d1d9a5742b120d2ce989aa4297107f3
Skye McLean <skyemclean87@gmail.com>
2024-02-02 12:18:20 -0800

    About updated

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ const About = () => {
     <div className='about'>
       <div className='entry'>
         <p className='entry-text'>
-          &nbsp;&nbsp;&nbsp;This is a submission based game reviews website built with
-          the purpose of documenting my gaming ratings. I get asked often as 
+          &nbsp;&nbsp;&nbsp;Skye's Game Reviews is a submission based game reviews website built with
+          the purpose of documenting game ratings. I get asked often as 
           a streamer what my sentiments or feelings are about a game and I'm often
           not the best at articulating those words the way I'd like to when asked.
           <br></br>
@@ -15,7 +15,7 @@ const About = () => {
           &nbsp;&nbsp;&nbsp;The other purpose of this website is to give my friends, viewers,
           community members and fellow gamers a place to submit their own reviews too! It's a fun hobby to practice writing
           and I have a lot of passionate fellow gamers in my circle who I hope will also help contribute to 
-          this site's content and purpose!
+          this site's content and purpose.
           <br></br>
           <br></br>
           &nbsp;&nbsp;&nbsp;If you want to submit a game review you can either use the built in submission form 
